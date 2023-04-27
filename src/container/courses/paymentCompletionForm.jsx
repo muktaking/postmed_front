d01df69f0258d3da81edfc4c2c2de44f5a2a689b
muktaking <yeakubinfire@gmail.com>
2023-04-27 13:09:04 +0600

    making a simple enrollment payment modal

--- src/container/courses/paymentCompletionForm.jsx
+++ src/container/courses/paymentCompletionForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Form, ListGroup, Modal } from 'react-bootstrap'
+import { Alert, Button, ListGroup, Modal } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { ContactConfig } from '../../config/contact.config'
 
@@ -20,48 +20,49 @@ export default function PaymentCompletionForm({
         <Modal.Body>
           <p>Thanks for enrolling this course. You are few steps apart.</p>
 
-          <ListGroup horizontal className='mb-2'>
-            <ListGroup.Item>Step One</ListGroup.Item>
+          <ListGroup className='mb-2'>
             <ListGroup.Item>
-              Please make Payment first with Bkash Personal (01737313915) |
-              Rocket Personal aacount (01737313915). For detail procedure go to{' '}
+              Step 1. Please make Payment first to Personal Bkash (01737313915)
+              | Rocket account (01737313915). For detail procedure go to{' '}
               <Link to='/payment'>Payment page</Link>{' '}
             </ListGroup.Item>
-          </ListGroup>
-
-          <ListGroup horizontal className='mb-2'>
-            <ListGroup.Item>Step Two</ListGroup.Item>
             <ListGroup.Item>
-              <Form.Group controlId='formBasicEmail'>
-                <Form.Label>
-                  After Payment, please fill the Bkash / Rocket Number (From
-                  which payemnt given) or TrxId (Transaction Id){' '}
-                </Form.Label>
-                <Form.Control type='text' placeholder='e.g. 017000111222' />
-              </Form.Group>
+              <p className='mt-2'>Step 2. Now Message at 01737313915</p>
+              <Alert variant='info'>
+                Write - Your name &lt;Space&gt; Your User ID / Your Email
+                &lt;Space&gt; The bKash Number (From which you send money)
+              </Alert>
+              <Alert variant='warning'>
+                You will Get Your user id at Topbar with golden color text or
+                your <Link to='/profile'>Profile</Link> marked with golden
+                background at the top section
+              </Alert>
             </ListGroup.Item>
           </ListGroup>
           <p>
-            Now, click the 'Confirm' button to complete this course enrollment.
+            Now, Click the 'Confirm' button to complete this course enrollment.
             Please wait until admin approves your payment. For any problems, go
             to <Link to='/help'>Help section.</Link>. Or contact us with{' '}
             <span>{ContactConfig.mobile}</span>
             <br />
             <span>{ContactConfig.email}</span>
           </p>
+          <div className='d-flex justify-content-center mt-2'>
+            <Button
+              variant='primary'
+              onClick={() => {
+                enrollmentHandler(course.id)
+                handleClose()
+              }}
+              size='lg'
+            >
+              Confirm
+            </Button>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
             Close
-          </Button>
-          <Button
-            variant='primary'
-            onClick={() => {
-              enrollmentHandler(course.id)
-              handleClose()
-            }}
-          >
-            Confirm
           </Button>
         </Modal.Footer>
       </Modal>
