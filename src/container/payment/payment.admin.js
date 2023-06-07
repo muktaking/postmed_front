ca6fcedd90729deeb8a34793e9ce18d0bfcaf4b1
muktaking <yeakubinfire@gmail.com>
2023-06-07 13:34:32 +0600

    payment refund added

--- src/container/payment/payment.admin.js
+++ src/container/payment/payment.admin.js
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import ReactJson from 'react-json-view'
 import { Button, Modal, Table } from 'react-bootstrap'
+import Pagination from 'react-js-pagination'
+import Refund from './refund'
+import { paginate } from '../../utils/paginate'
 
 export default function PaymentAdmin() {
   const [payments, setPayments] = useState(null)
+  const [refundForm, setRefundForm] = useState(null)
   const [show, setShow] = useState(false)
   const [paymentIdDetail, setPaymentIdDetail] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1)
+  const pageSize = 10
+
   useEffect(() => {
     axios
       .get(process.env.REACT_APP_SITE_URL + '/payment/')
@@ -18,7 +25,11 @@ export default function PaymentAdmin() {
       })
   }, [])
 
-  const handleClose = () => setShow(false)
+  const handleClose = () => {
+    setShow(false)
+    setPaymentIdDetail(null)
+    setRefundForm(null)
+  }
   const handleShow = () => setShow(true)
   const showPaymentIdDetails = (paymentId) => {
     handleShow()
@@ -34,6 +45,13 @@ export default function PaymentAdmin() {
       })
   }
 
+  const handleRefund = (paymentId, trxId, amount) => {
+    handleShow()
+    setRefundForm({ paymentId, trxId, amount })
+  }
+
+  const paginatedPayments = paginate(payments, currentPage, pageSize)
+
   return (
     <div>
       <Modal show={show} onHide={handleClose}>
@@ -42,6 +60,13 @@ export default function PaymentAdmin() {
         </Modal.Header>
         <Modal.Body>
           {paymentIdDetail && <ReactJson src={paymentIdDetail} name={null} />}
+          {refundForm && (
+            <Refund
+              paymentId={refundForm?.paymentId}
+              trxId={refundForm?.trxId}
+              amount={refundForm?.amount}
+            />
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
@@ -59,7 +84,8 @@ export default function PaymentAdmin() {
             <th>CustomerMsisdn</th>
             <th>Product Type</th>
             <th>ProductId</th>
-            <th>TxId</th>
+            <th>InvoiceId</th>
+            <th>TrxId</th>
             <th>Amount</th>
             <th>Ref</th>
             <th>PaymentGateway</th>
@@ -67,10 +93,11 @@ export default function PaymentAdmin() {
             <th>PaymentCreateTime</th>
             <th>PaymentExecuteTime</th>
             <th>Status</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {payments?.map((payment, index) => {
+          {paginatedPayments?.map((payment, index) => {
             return (
               <tr>
                 <td
@@ -78,16 +105,45 @@ export default function PaymentAdmin() {
                     showPaymentIdDetails(payment.id)
                   }}
                 >
-                  {index + 1}
+                  {index + 1 + (currentPage - 1) * pageSize}
                 </td>
                 {Object.keys(payment).map((key) => (
                   <td>{payment[key]}</td>
                 ))}
+                <td>
+                  <Button
+                    disabled={!payment.trxId}
+                    onClick={() => {
+                      handleRefund(
+                        payment.id,
+                        payment.trxId,
+                        payment.paymentAmount
+                      )
+                    }}
+                  >
+                    Make Refund
+                  </Button>
+                </td>
               </tr>
             )
           })}
         </tbody>
       </Table>
+      <div className='d-flex justify-content-center mt-3'>
+        <Pagination
+          activePage={currentPage}
+          itemsCountPerPage={pageSize}
+          totalItemsCount={payments ? payments.length : null}
+          pageRangeDisplayed={2}
+          onChange={(page) => {
+            setCurrentPage(page)
+          }}
+          itemClass='page-item'
+          linkClass='page-link'
+          prevPageText='Previous'
+          nextPageText='Next'
+        />
+      </div>
     </div>
   )
 }
---
+++ src/container/payment/refund.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from 'react'
+import axios from 'axios'
+import { Alert, Button, Form } from 'react-bootstrap'
+import ReactJson from 'react-json-view'
+import MultiSquareLoader from '../../components/customSpinner/multiSquare/multiSquareLoader'
+
+export default function Refund({ paymentId, trxId, amount }) {
+  const [loading, setLoading] = useState(false)
+  const [refundData, setRefundData] = useState(null)
+  const [refundRes, setRefundRes] = useState(null)
+  const amountRef = useRef()
+  const skuRef = useRef()
+  const reasonRef = useRef()
+  useEffect(() => {
+    setLoading(true)
+    axios
+      .get(
+        process.env.REACT_APP_SITE_URL + '/payment/bkash/refund/' + paymentId
+      )
+      .then(({ data }) => {
+        setLoading(false)
+        setRefundData(data)
+      })
+      .catch((error) => {
+        setLoading(false)
+        console.log(error)
+      })
+  }, [paymentId])
+
+  const handleRefund = () => {
+    setLoading(true)
+    axios
+      .post(
+        process.env.REACT_APP_SITE_URL + '/payment/bkash/refund/' + paymentId,
+        {
+          paymentId,
+          trxId,
+          amount,
+          sku: skuRef.current.value,
+          reason: reasonRef.current.value
+        }
+      )
+      .then(({ data }) => {
+        setLoading(false)
+        setRefundRes(data)
+      })
+      .catch(({ response }) => {
+        setLoading(false)
+        setRefundRes(response?.data.message)
+      })
+  }
+
+  return (
+    <div>
+      {loading && <MultiSquareLoader />}
+      {refundData && <ReactJson src={refundData} name={'OldData'} />}
+
+      {refundRes && <ReactJson src={refundRes} name={'RecentData'} />}
+      <Form>
+        <Form.Group controlId='formAmount'>
+          <Form.Label>Amount</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='500'
+            ref={amountRef}
+            defaultValue={amount}
+          />
+        </Form.Group>
+        <Form.Group controlId='formSku'>
+          <Form.Label>Sku</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Basic MD Course'
+            ref={skuRef}
+          />
+        </Form.Group>
+        <Form.Group controlId='formReason'>
+          <Form.Label>Reason</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Can not enroll'
+            ref={reasonRef}
+          />
+        </Form.Group>
+        <Button variant='primary' onClick={handleRefund}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  )
+}
